98b04eefbe87346e953642d0a274863e8c2e3373
bulmang <hmk113737@gmail.com>
2023-09-02 01:00:55 +0900

    feat: MainView 구현 (#1)

--- Toucher/Toucher/View/MainView/MainView.swift
+++ Toucher/Toucher/View/MainView/MainView.swift
@@ -7,158 +7,141 @@
 
 import SwiftUI
 
-struct TouchGesture: Identifiable,Hashable{
-    let id = UUID()
-    let name: String
-    var image: String
-    let subName: String
-}
-
 struct MainView: View {
-    
-    let brands: [TouchGesture] = [
-        .init(name: "두 번 누르기", image: "Primary", subName: "Double Tap"),
-        .init(name: "길게 누르기", image: "Primary", subName: "Long Tap"),
-        .init(name: "살짝 쓸기", image: "Primary", subName: "Swipe"),
-        .init(name: "끌어오기", image: "Primary", subName: "Drag"),
-        .init(name: "화면 움직이기", image: "Primary", subName: "Pan"),
-        .init(name: "확대 축소하기", image: "Primary", subName: "Zoom in, out"),
-        .init(name: "회전하기", image: "Primary", subName: "Rotate"),
-    ]
-    
-    @State private var navigationPath = [TouchGesture]()
-    
+    @State private var navigationPath = [MainViewModel]()
     @State private var showFullStack = false
     
-    
-    
+    private let isSE = DeviceManager.shared.iPhoneSE()
     
     var body: some View {
         NavigationStack(path: $navigationPath) {
             ZStack {
                 Color.white
                     .ignoresSafeArea()
-                VStack(alignment: .leading,spacing: 0) {
-                    ScrollView(showsIndicators: false){
-                        ForEach(brands) { brand in
-                            NavigationLink(value: brand) {
-                                Rectangle()
-                                    .foregroundColor(Color("\(brand.image)"))
-                                    .frame(height: 115)
-                                    .frame(maxWidth: .infinity)
-                                    .cornerRadius(10)
-                                    .padding(.horizontal,16)
-                                    .shadow(radius: 4,x:0,y:0)
-                                    .overlay(
+                VStack(alignment: .leading, spacing: 0) {
+                    ForEach(gestures) { gesture in
+                        NavigationLink(value: gesture) {
+                            LinearGradient(
+                                colors: [Color("Primary"), Color("Secondary")],
+                                startPoint: UnitPoint(x: 0.09, y: 0.5),
+                                endPoint: UnitPoint(x: 1.65, y: 0.5)
+                            )
+                            .frame(height: isSE ? 77 : 88)
+                            .frame(maxWidth: .infinity)
+                            .cornerRadius(10)
+                            .overlay(
+                                HStack {
+                                    Image(gesture.iconName)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 60, height: 60)
+                                    
+                                    Text(gesture.name)
+                                        .font(.customButtonText())
+                                        .foregroundColor(.white)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .padding(.leading, 36)
+                                    
+                                    Image(systemName: "chevron.forward")
+                                        .font(.system(size: 18))
+                                        .foregroundColor(Color.white)
+                                        .fontWeight(.bold)
                                         
-                                        HStack {
-                                            VStack() {
-                                                
-                                                Text(brand.name)
-                                                    .font(.customTitle())
-                                                    .foregroundColor(.white)
-                                                    .frame(width: 280,alignment: .leading)
-                                                
-                                                Text(brand.subName)
-                                                    .foregroundColor(.white)
-                                                    .frame(width: 280,alignment: .leading)
-                                                    
-                                            }
-                                            .padding(.leading,36)
-                                            Image(systemName: "chevron.forward")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 11, height: 18)
-                                                .foregroundColor(Color.white)
-                                                .fontWeight(.bold)
-                                                .padding(.trailing,50)
-                                        }
-                                        
-                                    )
-                                    .padding(.bottom,12)
-
-                            }
+                                }
+                                    .padding(.horizontal, 18)
+                            )
+                            .padding(.horizontal, 16)
+                            .padding(.bottom, 6)
                         }
                     }
-                    .navigationDestination(for: TouchGesture.self) { brand in
-                        VStack(spacing:0){
-                            Button {
-                                navigationPath = []
-                            } label: {
-                                HStack(spacing: 5) {
-    
-                                    Image(systemName: "chevron.backward")
-                                        .font(.system(size: 17))
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(Color("Primary"))
-    
-                                    Text("이전으로")
-                                        .font(.system(size: 16))
-                                        .foregroundColor(Color("Primary"))
-    
-                                    Spacer()
-    
-                                    Text("\(brand.name)")
-                                        .font(.system(size: 17))
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(Color("GR1"))
-    
-                                    Spacer()
-    
-                                    Spacer()
-                                }
-                                
-                                .frame(maxWidth: .infinity,alignment: .leading)
-                                .padding(.horizontal,8)
-                                .padding(.vertical,11)
-                                .overlay(
-                                                Rectangle()
-                                                    .frame(height: 0.5)
-                                                    .foregroundColor(Color("GR3")),
-                                                alignment: .bottom
-                                        )
-                                .background(Color.clear)
-                                
-    
-                            }
-                            viewForBrand(brand)
+                    .navigationDestination(for: MainViewModel.self) { gesture in
+                        VStack(spacing: 0) {
+                            customHeader(for: gesture)
+                            
+                            viewForgesture(gesture)
                         }
                     }
                 }
             }
-        }.accentColor(.red)
-            .onAppear{
-                 UserDefaults.standard.set(true, forKey: "goToMain")
-             }
-        
-        
+        }
+        .accentColor(.red)
+        .onAppear {
+            UserDefaults.standard.set(true, forKey: "goToMain")
+        }
     }
-    func viewForBrand(_ brand: TouchGesture) -> AnyView {
-        switch brand.name {
-        case "길게 누르기":
-            return AnyView(Color.gray)
+    
+    func viewForgesture(_ gesture: MainViewModel) -> AnyView {
+        switch gesture.name {
         case "두 번 누르기":
-            return AnyView(Color.gray)
-        case "화면 움직이기":
-            return AnyView(Color.gray)
-        case "끌어오기":
-            return AnyView(Color.gray)
+            return AnyView(Color.gray.navigationBarBackButtonHidden())
+        case "길게 누르기":
+            return AnyView(Color.gray.navigationBarBackButtonHidden())
         case "살짝 쓸기":
-            return AnyView(Color.gray)
+            return AnyView(Color.gray.navigationBarBackButtonHidden())
+        case "끌어오기":
+            return AnyView(Color.gray.navigationBarBackButtonHidden())
+        case "화면 움직이기":
+            return AnyView(Color.gray.navigationBarBackButtonHidden())
         case "확대 축소하기":
-            return AnyView(Color.gray)
+            return AnyView(Color.gray.navigationBarBackButtonHidden())
         case "회전하기":
-            return AnyView(Color.gray)
+            return AnyView(Color.gray.navigationBarBackButtonHidden())
         default:
             return AnyView(Color.gray)
         }
     }
     
-    
+    @ViewBuilder
+    func customHeader(for gesture: MainViewModel) -> some View {
+        Button {
+            navigationPath = []
+        } label: {
+            HStack(spacing: 5) {
+                
+                Image(systemName: "chevron.backward")
+                    .font(.system(size: 17))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color("Primary"))
+                
+                Text("이전으로")
+                    .font(.system(size: 16))
+                    .foregroundColor(Color("Primary"))
+                
+                Spacer()
+                
+                Text("\(gesture.name)")
+                    .font(.system(size: 17))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color("GR1"))
+                
+                Spacer()
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 8)
+            .padding(.vertical, 11)
+            .overlay(
+                Rectangle()
+                    .frame(height: 0.5)
+                    .foregroundColor(Color("GR3")),
+                alignment: .bottom
+            )
+            .background(Color.clear)
+        }
+    }
 }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        Group {
+            MainView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+                .previewDisplayName("iPhone SE")
+            
+            MainView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
+                .previewDisplayName("iPhone 14")
+        }
     }
 }
