bd6f511e3b68224875ddd2d924461936cb3ca569
hyunjun <hyunjuntyler@gmail.com>
2023-09-06 19:41:37 +0900

    feat: Rotation 뷰 추가 (#16)

---
+++ Toucher/Toucher/Gesture/Rotate/RotateExampleView.swift
@@ -0,0 +1,91 @@
+//
+//  RotateExampleView.swift
+//  Toucher
+//
+//  Created by hyunjun on 2023/09/06.
+//
+
+import SwiftUI
+
+struct RotateExampleView: View {
+    @State private var isTapped = false
+    @State private var isSuceess = false
+    @State private var isOneTapped = false
+    
+    @State private var currentAmount = Angle.degrees(0)
+    
+    @Namespace var namespace
+        
+    var body: some View {
+        ZStack {
+            if isOneTapped && !isSuceess {
+                Color.accentColor.opacity(0.5).ignoresSafeArea()
+            }
+            VStack {
+                Text(isSuceess ? "잘하셨어요!\n" : "두 손가락을 원 위에 대고\n회전시켜볼까요?")
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(10)
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top, 30)
+                if isSuceess {
+                    Image("ch_default")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 168)
+                        .frame(maxHeight: .infinity)
+                } else {
+                    Image("ch_default")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 168)
+                        .frame(maxHeight: .infinity)
+                        .rotationEffect(.degrees(-180) + currentAmount)
+                        .gesture(
+                            RotationGesture()
+                                .onChanged { angle in
+                                    currentAmount = angle
+                                    withAnimation {
+                                        isTapped = true
+                                    }
+                                }
+                                .onEnded {_ in
+                                    if currentAmount.degrees < -150 || currentAmount.degrees > 150 {
+                                        withAnimation {
+                                            isSuceess = true
+                                        }
+                                    } else {
+                                        print(currentAmount.degrees)
+                                        isSuceess = false
+                                    }
+                                })
+                }
+                Group {
+                    Text("화면의 물체를")
+                    + Text(" 뒤집어 보고")
+                        .bold()
+                    + Text("\n싶을 때 사용해요.")
+                }
+                .multilineTextAlignment(.center)
+                .lineSpacing(10)
+                .foregroundColor(isTapped ? .clear : .gray)
+                .font(.title)
+                .padding(.bottom, 80)
+            }
+            
+        }
+        .onAppear {
+            isTapped = false
+            isSuceess = false
+            isOneTapped = false
+            currentAmount = .degrees(0)
+        }
+    }
+}
+
+struct RotateExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RotateExampleView()
+    }
+}
---
+++ Toucher/Toucher/Gesture/Rotate/RotationPracticeView.swift
@@ -0,0 +1,65 @@
+//
+//  RotationPracticeView.swift
+//  Toucher
+//
+//  Created by hyunjun on 2023/09/06.
+//
+
+import SwiftUI
+
+struct RotationPracticeView: View {
+    @State private var isTapped = false
+    @State private var isSuceess = false
+    @State private var isOneTapped = false
+    
+    @State private var currentAmount = Angle.degrees(0)
+    
+    @Namespace var namespace
+    
+    var body: some View {
+        ZStack {
+            Image("RotationMap")
+                .resizable()
+                .scaledToFit()
+                .rotationEffect(currentAmount)
+                .gesture(
+                    RotationGesture()
+                        .onChanged { angle in
+                            currentAmount = angle
+                            withAnimation {
+                                isTapped = true
+                            }
+                        }
+                        .onEnded {_ in
+                            if currentAmount.degrees < -30 || currentAmount.degrees > 30 {
+                                withAnimation {
+                                    isSuceess = true
+                                }
+                            } else {
+                                print(currentAmount.degrees)
+                                isSuceess = false
+                            }
+                        })
+            Text(isSuceess ? "잘하셨어요!" : "지도를 회전시켜 볼까요?")
+                .foregroundColor(.primary)
+                .multilineTextAlignment(.center)
+                .lineSpacing(10)
+                .font(.largeTitle)
+                .bold()
+                .padding(.top, 30)
+                .frame(maxHeight: .infinity, alignment: .top)
+            
+        }
+        .onAppear {
+            isTapped = false
+            isSuceess = false
+            isOneTapped = false
+        }
+    }
+}
+
+struct RotationPracticeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RotationPracticeView()
+    }
+}
