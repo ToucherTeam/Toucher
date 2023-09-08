4c7bd010d3cedcc167f169d5a7b4526386dfebe6
hyunjun <hyunjuntyler@gmail.com>
2023-09-08 16:22:32 +0900

    design: Rotation 위 네비게이션 바 수정 (#33)

--- Toucher/Toucher/Gesture/Rotate/RotationPracticeView.swift
+++ Toucher/Toucher/Gesture/Rotate/RotationPracticeView.swift
@@ -17,80 +17,93 @@ struct RotationPracticeView: View {
     @Namespace var namespace
     
     var body: some View {
-        ZStack {
-            Image("Map")
-                .resizable()
-                .scaledToFill()
-                .frame(maxHeight: .infinity)
-                .edgesIgnoringSafeArea(.bottom)
-                .rotationEffect(currentAmount)
-                .gesture(
-                    RotationGesture()
-                        .onChanged { angle in
-                            currentAmount = angle
-                            withAnimation {
-                                isTapped = true
-                            }
-                        }
-                        .onEnded {_ in
-                            if currentAmount.degrees < -30 || currentAmount.degrees > 30 {
+            ZStack {
+                Image("Map")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxHeight: .infinity)
+                    .edgesIgnoringSafeArea(.bottom)
+                    .rotationEffect(currentAmount)
+                    .gesture(
+                        RotationGesture()
+                            .onChanged { angle in
+                                currentAmount = angle
                                 withAnimation {
-                                    isSuceess = true
+                                    isTapped = true
                                 }
-                            } else {
-                                print(currentAmount.degrees)
-                                isSuceess = false
                             }
-                        })
-            Text(isSuceess ? "잘하셨어요!" : "지도를 회전시켜 볼까요?")
-                .foregroundColor(.primary)
-                .multilineTextAlignment(.center)
-                .lineSpacing(10)
-                .font(.largeTitle)
-                .bold()
-                .padding(.top, 30)
-                .frame(maxHeight: .infinity, alignment: .top)
-            
-            if isSuceess {
-                ToucherNavigationLink(label: "완료") {
-                    FinalView(gestureTitle: "회전하기")
-                        .padding(.bottom, 13)
-                        .overlay(
-                            Rectangle()
-                                .frame(height: 0.5)
-                                .foregroundColor(Color("GR3")),
-                            alignment: .top
-                        )
-                        .toolbar {
-                            ToolbarItem(placement: .principal) {
-                                CustomToolbar()
-                            }
+                            .onEnded {_ in
+                                if currentAmount.degrees < -30 || currentAmount.degrees > 30 {
+                                    withAnimation {
+                                        isSuceess = true
+                                    }
+                                } else {
+                                    print(currentAmount.degrees)
+                                    isSuceess = false
+                                }
+                            })
+                GeometryReader { geometry in
+                    Color.clear
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .overlay(alignment: .top) {
+                            Text("")
+                                .frame(height: geometry.safeAreaInsets.top)
+                                .frame(maxWidth: .infinity)
+                                .background {
+                                    Color.white
+                                }
+                                .edgesIgnoringSafeArea(.top)
                         }
                 }
-                .frame(maxWidth: UIScreen.main.bounds.width)
+                Text(isSuceess ? "잘하셨어요!" : "지도를 회전시켜 볼까요?")
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(10)
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top, 30)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                
+                if isSuceess {
+                    ToucherNavigationLink(label: "완료") {
+                        FinalView(gestureTitle: "회전하기")
+                            .padding(.bottom, 13)
+                            .overlay(
+                                Rectangle()
+                                    .frame(height: 0.5)
+                                    .foregroundColor(Color("GR3")),
+                                alignment: .top
+                            )
+                            .toolbar {
+                                ToolbarItem(placement: .principal) {
+                                    CustomToolbar()
+                                }
+                            }
+                    }
+                    .frame(maxWidth: UIScreen.main.bounds.width)
+                }
+            }
+            .overlay(
+                Rectangle()
+                    .frame(height: 0.5)
+                    .foregroundColor(Color("GR3")),
+                alignment: .top
+            )
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    CustomToolbar()
+                }
+            }
+            .onAppear {
+                isTapped = false
+                isSuceess = false
+                isOneTapped = false
             }
         }
-        .overlay(
-            Rectangle()
-                .frame(height: 0.5)
-                .foregroundColor(Color("GR3")),
-            alignment: .top
-        )
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                CustomToolbar()
-            }
-        }
-        .onAppear {
-            isTapped = false
-            isSuceess = false
-            isOneTapped = false
+    }
+    
+    struct RotationPracticeView_Previews: PreviewProvider {
+        static var previews: some View {
+            RotationPracticeView()
         }
     }
-}
-
-struct RotationPracticeView_Previews: PreviewProvider {
-    static var previews: some View {
-        RotationPracticeView()
-    }
-}
