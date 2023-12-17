7348317ab02561382d5929c3cc8056955bb4f3d5
bulmang <hmk113737@gmail.com>
2023-12-18 05:27:44 +0900

    feat: HelpButton Componenet에 FullScreenCover 연결

--- Toucher/Toucher/Component/Buttons/HelpButton.swift
+++ Toucher/Toucher/Component/Buttons/HelpButton.swift
@@ -14,6 +14,7 @@ enum HelpButtonStyle {
 
 struct HelpButton: View {
     @State private var animate = false
+    @State private var isFullScreenPresented = false
     var style: HelpButtonStyle
     var action: () -> Void
     
@@ -40,7 +41,10 @@ struct HelpButton: View {
     }
     
     var body: some View {
-        Button(action: action) {
+        Button(action: {
+            isFullScreenPresented.toggle()
+            action()
+        }) {
             Text("도움이 필요하신가요?")
                 .font(.customButton)
                 .foregroundStyle(textColor)
@@ -61,16 +65,64 @@ struct HelpButton: View {
                     }
                 }
         }
+        .fullScreenCover(isPresented: $isFullScreenPresented, content: {
+            // Your full-screen content goes here
+            ZStack {
+                Color.black
+                    .ignoresSafeArea()
+                    .overlay(alignment: .top) {
+                            ProgressView()
+                                .progressViewStyle(CustomProgressViewStyle())
+                                .padding(.leading, 15)
+                    }
+                Image("Album3")
+                    .ignoresSafeArea()
+            }
+            .overlay(alignment: .topTrailing) {
+                Button(action: {
+                    isFullScreenPresented.toggle()
+                }, label: {
+                    Image(systemName: "xmark")
+                        .frame(height: 14)
+                        .foregroundStyle(.white)
+                })
+            }
+        })
     }
 }
 
-#Preview {
-    VStack {
-        HelpButton(style: .primary) {
-            // action here
+struct HelpButton_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HelpButton(style: .primary) {
+                // action here
+            }
+            HelpButton(style: .secondary) {
+                // action here
+            }
         }
-        HelpButton(style: .secondary) {
-            // action here
+    }
+}
+
+
+struct CustomProgressViewStyle: ProgressViewStyle {
+    let height: CGFloat = 10
+    var foregroundColor: Color = .customSecondary
+    var backgroundColor: Color = .customWhite
+    
+    func makeBody(configuration: Configuration) -> some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .frame(width: 306, height: height)
+                    .clipShape(.capsule)
+                    .foregroundColor(backgroundColor)
+                
+                Rectangle()
+                    .frame(width: geometry.size.width - 90, height: height)
+                    .clipShape(.capsule)
+                    .foregroundColor(foregroundColor)
+            }
         }
     }
 }
