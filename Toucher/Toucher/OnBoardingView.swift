5a04e290b301ab934deba78150eff042f1810493
bulmang <hmk113737@gmail.com>
2023-12-20 04:26:04 +0900

    feat: OnBoardingView 구현

--- Toucher/Toucher/OnBoardingView.swift
+++ Toucher/Toucher/OnBoardingView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct OnBoardingView: View {
+    @AppStorage("first") private var isFirst = true
+    
+    @State private var isSuccess = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.customWhite
+            VStack(spacing: 0) {
+                Text(isSuccess ? "잘하셨어요!" : "저를 한 번\n눌러보세요!")
+                    .font(.customTitle)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 96)
+                    .padding(.vertical, 24)
+                    .background(Color.customBG1)
+                    .cornerRadius(12)
+                    .shadow(color: Color.customShadow, radius: 5, x: 0, y: 4)
+                Image("Polygon 1")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 165)
+                    .shadow(color: Color.customShadow, radius: 5, x: 0, y: 4)
+                
+                TapButton(isSuccess: $isSuccess)
+                    .padding(.top, 56)
+            }
+            .onChange(of: isSuccess) { value in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    withAnimation {
+                        self.isFirst = false
+                    }
+                }
+            }
+        }
     }
 }
 
