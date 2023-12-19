155259081ac1e1d8bd99159272d17ba42f6f9378
bulmang <hmk113737@gmail.com>
2023-12-20 04:15:35 +0900

    feat: TapButton Component 구현

---
+++ Toucher/Toucher/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Toucher
+//
+//  Created by bulmang on 12/19/23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
