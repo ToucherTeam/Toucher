86bc87bbed54b7a69f294b33b8a5357dbbb9321e
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2024-01-04 16:50:03 +0900

    feat: ExampleView ViewModel 적용 (#68)

--- Toucher/Toucher/View/GestureView/LongTap/LongTapExampleView.swift
+++ Toucher/Toucher/View/GestureView/LongTap/LongTapExampleView.swift
@@ -8,66 +8,48 @@
 import SwiftUI
 
 struct LongTapExampleView: View {
-    @State private var isTapStart = false
-    @State private var isSuccess = false
-    @State private var isFail = false
-    @State private var navigate = false
-        
-    @State private var animate = [false, false, false]
-    
+    @StateObject private var longTapVM = LongTapViewModel()
+            
     var body: some View {
         ZStack {
-            if isFail && !isSuccess {
+            if longTapVM.isFail && !longTapVM.isSuccess {
                 Color.customSecondary.ignoresSafeArea()
             }
             VStack {
-                CustomToolbar(title: "길게 누르기", isSuccess: isSuccess)
+                CustomToolbar(title: "길게 누르기", isSuccess: longTapVM.isSuccess)
 
-                Text(isSuccess ? "성공!\n" : isFail ? "조금 더 길게 꾹 \n눌러주세요!" : "1초동안 길게\n눌러볼까요?")
-                    .foregroundColor(isFail && !isSuccess ? .white : .primary)
+                Text(longTapVM.isSuccess ? "성공!\n" : longTapVM.isFail ? "조금 더 길게 꾹 \n눌러주세요!" : "1초동안 길게\n눌러볼까요?")
+                    .foregroundColor(longTapVM.isFail && !longTapVM.isSuccess ? .white : .primary)
                     .multilineTextAlignment(.center)
                     .lineSpacing(10)
                     .font(.customTitle)
                     .padding(.top, 40)
                 
-                LongPressButton(isSuccess: $isSuccess, isFail: $isFail)
+                LongPressButton(isSuccess: $longTapVM.isSuccess, isFail: $longTapVM.isFail)
                     .padding(.bottom)
                     .frame(maxHeight: .infinity)
                     .overlay {
-                        if isSuccess {
+                        if longTapVM.isSuccess {
                             ConfettiView()
                         }
                     }
 
-                HelpButton(style: isFail ? .primary : .secondary, currentViewName: "LongTapExampleView") {
+                HelpButton(style: longTapVM.isFail ? .primary : .secondary, currentViewName: "LongTapExampleView") {
                     
                 }
-                .opacity(isSuccess ? 0 : 1)
-                .animation(.easeInOut, value: isSuccess)
+                .opacity(longTapVM.isSuccess ? 0 : 1)
+                .animation(.easeInOut, value: longTapVM.isSuccess)
             }
-            .onChange(of: isSuccess) { _ in
-                if isSuccess {
-                    HapticManager.notification(type: .success)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                        navigate = true
-                    }
-                }
-            }
-            .navigationDestination(isPresented: $navigate) {
+            .modifier(SuccessNavigateModifier(navigate: $longTapVM.navigate, isSuccess: longTapVM.isSuccess))
+            .navigationDestination(isPresented: $longTapVM.navigate) {
                 LongTapPracticeView1()
                     .toolbar(.hidden, for: .navigationBar)
             }
         }
         .onAppear {
-            reset()
+            longTapVM.reset()
         }
         .toolbar(.hidden, for: .navigationBar)
-    }
-        
-    private func reset() {
-        isTapStart = false
-        isSuccess = false
-        isFail = false
     }
 }
 
