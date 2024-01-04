69eff146a93da3b63831f9001043af7b0165aa52
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2024-01-04 16:41:58 +0900

    feat: Modifier 생성 (#68)

---
+++ Toucher/Toucher/Modifier/EndNavigateModifier.swift
@@ -0,0 +1,28 @@
+//
+//  EndNavigateModifier.swift
+//  Toucher
+//
+//  Created by Hyunjun Kim on 1/4/24.
+//
+
+import SwiftUI
+
+struct EndNavigateModifier: ViewModifier {
+    @StateObject private var navigationManager = NavigationManager.shared
+    
+    @Binding var navigate: Bool
+    
+    let isSuccess: Bool
+    
+    func body(content: Content) -> some View {
+        content.onChange(of: isSuccess) { _ in
+            if isSuccess {
+                HapticManager.notification(type: .success)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    navigationManager.navigate = false
+                    navigationManager.updateGesture()
+                }
+            }
+        }
+    }
+}
---
+++ Toucher/Toucher/Modifier/SuccessNavigateModifier.swift
@@ -0,0 +1,25 @@
+//
+//  SuccessNavigateModifier.swift
+//  Toucher
+//
+//  Created by Hyunjun Kim on 1/4/24.
+//
+
+import SwiftUI
+
+struct SuccessNavigateModifier: ViewModifier {
+    @Binding var navigate: Bool
+    
+    let isSuccess: Bool
+
+    func body(content: Content) -> some View {
+        content.onChange(of: isSuccess) { _ in
+            if isSuccess {
+                HapticManager.notification(type: .success)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    navigate = true
+                }
+            }
+        }
+    }
+}
