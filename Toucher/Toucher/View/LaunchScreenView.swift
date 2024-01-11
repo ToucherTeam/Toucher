116467b066615f2ed08a5079e6ae5abb24efb9a9
hyunjun <hyunjuntyler@gmail.com>
2024-01-11 23:40:16 +0900

    feat: 익명 로그인 구현 (#79)

--- Toucher/Toucher/View/LaunchScreenView.swift
+++ Toucher/Toucher/View/LaunchScreenView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 struct LaunchScreenView: View {
     var body: some View {
@@ -17,7 +20,26 @@ struct LaunchScreenView: View {
                 .scaledToFit()
                 .padding(.horizontal, 100)
         }
+        .onAppear {
+            signAnonymously()
+        }
         .ignoresSafeArea()
+    }
+    
+    private func signAnonymously() {
+        guard let user = Auth.auth().currentUser else {
+            Auth.auth().signInAnonymously { authResult, error in
+                if let error = error {
+                    print("error \(error.localizedDescription)")
+                    return
+                }
+                guard let user = authResult?.user else { return }
+                print("sign in \(user.uid)")
+            }
+            return
+        }
+        
+        print("log in \(user.uid)")
     }
 }
 
