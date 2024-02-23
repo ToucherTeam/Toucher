be17a7bf33dd25f043ac46ef7a8b4db9a9faff1e
bulmang <hmk113737@gmail.com>
2024-02-23 15:16:22 +0900

    chore: Analytics Data 이름 변경 (#94)

--- Toucher/Toucher/Component/Header/CustomToolbar.swift
+++ Toucher/Toucher/Component/Header/CustomToolbar.swift
@@ -70,7 +70,7 @@ struct CustomToolbar: View {
             .overlay(alignment: .leading) {
                 Button {
                     dismiss()
-                    AnalyticsManager.shared.logEvent(name: "\(viewName)BackButtonCount")
+                    AnalyticsManager.shared.logEvent(name: "\(viewName)_BackButtonCount")
                 } label: {
                     HStack(spacing: 3) {
                         Image(systemName: "chevron.backward")
